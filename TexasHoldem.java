ceb235dc4a738b44a5de474c30c6b63d46cb8a83
xpreds <akduranyildiz@hotmail.com>
2015-12-31 11:11:58 +0200

    Started Deck and Hand Classes
    Just copied and pasted the old classes. Need to change some code to fit
    new project.

--- TexasHoldem.java
+++ TexasHoldem.java
@@ -1,5 +1,40 @@
 public class TexasHoldem {
+  
   public static void main(String[] args) {
+    
+  }
+  
+  public void Poker() {
+    // The Preflop
+    Preflop();
+    //The Flop
+    Flop();
+    //The Turn
+    Turn();
+    //The River
+    River();
+    //The Showdown
+    Showdown();
+   
+  }
+  
+  public void Preflop() {
+    
+  }
+  
+  public void Flop() {
+    
+  }
+  
+  public void Turn() {
+    
+  }
+  
+  public void River() {
+    
+  }
+  
+  public void Showdown() {
     
   }
 }
